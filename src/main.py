2e7d67fe7b4af6e22b9914ba2351b4a7481e064a
melMass <mel@melmassadian.com>
2024-01-15 18:08:51 +0100

    fix: ✨ update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,37 @@
+import glob
+import json
 import os
-for env in os.environ:
-    print(f"{env} : {os.environ[env]}")
+import subprocess
 
-print("----------")
-print(sys.argv)
+
+def build_wheels():
+    # - list
+    installed_packages = (
+        subprocess.check_output(["pip", "freeze"]).decode().splitlines()
+    )
+
+    # - install the wheel package
+    subprocess.check_call(["pip", "install", "wheel"])
+
+    # -  build each
+    wheel_paths = []
+    for package in installed_packages:
+        package_name = package.split("==")[
+            0
+        ]  # get the package name, ignore the version
+        subprocess.check_call(["pip", "wheel", "--wheel-dir=wheels", package_name])
+        wheel_paths.extend(glob.glob(f"wheels/{package_name}*.whl"))
+
+    return wheel_paths
+
+
+def main():
+    wheel_paths = build_wheels()
+    output = f"wheel_paths={json.dumps(wheel_paths)}"
+    # append the output to the $GITHUB_OUTPUT file
+    with open(os.getenv("GITHUB_OUTPUT"), "a") as f:
+        f.write(output)
+
+
+if __name__ == "__main__":
+    main()
